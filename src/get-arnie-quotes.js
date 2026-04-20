550a436e615a039cc4844fbb714e874303dd9e98
Derek Overlock <overlock.derek@gmail.com>
2026-04-20 19:25:45 +1000

    implement getArnieQuotes fn

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,26 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const fetchSingleQuote = async (url) => {
+  const response = await httpGet(url);
+
+  // Possible TODO - JSON parsing error handling (skipped this as the Tips said to keep solution as simple as possible)
+  const body = JSON.parse(response.body);
+
+  if (response.status === 200) {
+    return {
+      "Arnie Quote": body.message,
+    };
+  } else {
+    return {
+      FAILURE: body.message,
+    };
+  }
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const quotes = urls.map((url) => fetchSingleQuote(url));
+
+  return Promise.all(quotes);
 };
 
 module.exports = {
